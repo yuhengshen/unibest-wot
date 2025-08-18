f967236cb381b31f7f7765d9a980359a84912af1
yuhengshen <github-action@actions-template-sync.noreply.github.com>
2025-08-18 00:17:43 +0000

    chore(template): merge template changes :up:
    Signed-off-by: yuhengshen <github-action@actions-template-sync.noreply.github.com>

--- src/layouts/tabbar.vue
+++ src/layouts/tabbar.vue
@@ -1,12 +1,16 @@
 <script lang="ts" setup>
 import type { ConfigProviderThemeVars } from 'wot-design-uni'
-import FgTabbar from './fg-tabbar/fg-tabbar.vue'
+import FgTabbar from '@/tabbar/index.vue'
 
 const themeVars: ConfigProviderThemeVars = {
   // colorTheme: 'red',
   // buttonPrimaryBgColor: '#07c160',
   // buttonPrimaryColor: '#07c160',
 }
+const testUniLayoutExposedData = ref('testUniLayoutExposedData')
+defineExpose({
+  testUniLayoutExposedData,
+})
 </script>
 
 <template>
---
+++ src/tabbar/store.ts
@@ -0,0 +1,32 @@
+import { tabbarList } from './config'
+
+/**
+ * 自定义 tabbar 的状态管理，原生 tabbar 无需关注本文件
+ * tabbar 状态，增加 storageSync 保证刷新浏览器时在正确的 tabbar 页面
+ * 使用reactive简单状态，而不是 pinia 全局状态
+ */
+export const tabbarStore = reactive({
+  curIdx: uni.getStorageSync('app-tabbar-index') || 0,
+  prevIdx: uni.getStorageSync('app-tabbar-index') || 0,
+  setCurIdx(idx: number) {
+    this.curIdx = idx
+    uni.setStorageSync('app-tabbar-index', idx)
+  },
+  setAutoCurIdx(path: string) {
+    const index = tabbarList.findIndex(item => item.pagePath === path)
+    // console.log('index:', index, path)
+    // console.log('tabbarList:', tabbarList)
+    if (index !== -1) {
+      this.setCurIdx(index)
+    }
+    else {
+      this.setCurIdx(0)
+    }
+  },
+  restorePrevIdx() {
+    if (this.prevIdx === this.curIdx)
+      return
+    this.setCurIdx(this.prevIdx)
+    this.prevIdx = uni.getStorageSync('app-tabbar-index') || 0
+  },
+})
